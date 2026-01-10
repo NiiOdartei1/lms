25d76e1002626976f8ff1cddcc5fc2c9a185244b
Christ Pentecostal Praise Family <89534730+NiiOdartei1@users.noreply.github.com>
2026-01-10 13:00:33 +0000

    Update chat_routes.py

--- chat_routes.py
+++ chat_routes.py
@@ -1,7 +1,7 @@
 from flask import Blueprint, render_template, request, jsonify
 from flask_login import login_required, current_user
 from flask_socketio import emit, join_room
-from utils.extensions import db, socketio
+from utils.extensions import socketio
 from models import Admin, Conversation, ConversationParticipant, Message, MessageReaction, SchoolClass, User, ParentChildLink
 from datetime import datetime
 import json
