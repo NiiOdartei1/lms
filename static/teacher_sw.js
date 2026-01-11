71737f4e01fc5585ba34ad8b6e8ff14df49e549a
Christ Pentecostal Praise Family <89534730+NiiOdartei1@users.noreply.github.com>
2026-01-11 17:29:57 +0000

    Update teacher_sw.js

--- static/teacher_sw.js
+++ static/teacher_sw.js
@@ -4,21 +4,11 @@ const CORE_ASSETS = [
   "/teacher/dashboard"
 ];
 
-self.addEventListener("install", event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS))
-  );
+self.addEventListener("install", () => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
-  event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
-      )
-    )
-  );
+self.addEventListener("activate", () => {
   self.clients.claim();
 });
 
